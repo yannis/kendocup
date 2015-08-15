5d335196fb6a967731a86c6c3cec9eb54ca37146
Yannis Jaquet <yannis.jaquet@unige.ch>
2015-08-15 18:49:04 +0200

    upgrade translation script

--- lib/translate.rb
+++ lib/translate.rb
@@ -4,6 +4,8 @@ module Translate
       define_method a.to_s do
         if I18n.locale.to_s == 'fr'
           return eval("#{a.to_s}_fr").blank? ? eval("#{a.to_s}_en") : eval("#{a.to_s}_fr")
+        elsif I18n.locale.to_s == 'de'
+          return eval("#{a.to_s}_de").blank? ? eval("#{a.to_s}_en") : eval("#{a.to_s}_de")
         else
           return eval("#{a.to_s}_en")
         end
